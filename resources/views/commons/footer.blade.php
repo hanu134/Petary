8aba53b223bee028bbe178e1b3ce7dceb221d8b3
ezra.mcavoy <miximaxa13427@gmail.com>
2019-11-30 06:48:17 +0000

    top page

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+    <div class="mt-5">
+        <small>&copy; 2019 Petary, Inc.</small>    
+    </div>
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="text-center">
+        <h1 class="text-muted mt-5 mb-4">Petary</h1>
+    </div>
+</header>
