8e4e695b8c1b2d6521ea5683e61a7c4d17f51912
ezra.mcavoy <miximaxa13427@gmail.com>
2019-12-09 14:26:25 +0000

    Procfile

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,9 @@
 @extends("layouts.page")
 
 <header class="fixed-top mb-3">
-    <nav class="navbar navbar-expand navbar-light bg-light p-3">
+    <nav class="navbar navbar-light bg-light p-3">
         <a href="http://2165fca39035409d8d52b5e54b0de824.vfs.cloud9.us-east-1.amazonaws.com/logout"><i class="fas fa-sign-out-alt fa-flip-horizontal fa-2x icon"></i></a>
-        <span class=" mx-auto navbar-text navbar-right">{{ $user->name }}</span>
+        <span class=" mx-auto navbar-text navbar-right username">{{ $user->name }}</span>
         <i class="fas fa-edit fa-2x icon"></i>
 
     </nav>
@@ -37,21 +37,15 @@
         
         <div class="w-100"></div>
 
-        
-        <div class="col-">
-            <p><i class="fas fa-paw paw border border-dark rounded-circle p-2"></i></p>
-        </div>
+        <p><i class="fas fa-paw paw border border-dark rounded-circle p-2"></i></p>
         <div class="w-100"></div>
-        <div class="col- mb-5">
-            <h4>{{ $user->name }}</h4>
-        </div>
-        
-            <div class="w-100"></div>
+        <h4 class="mb-5">{{ $user->name }}</h4>
+        <div class="w-100"></div>
             
     </div>
             
     <div class="row justify-content-center mb-5">
-        <button type="button" class="btn btn-info mr-5">フォロー <span class="badge badge-light badge-pill">32</span></button>
+        <button type="button" class="btn btn-info mr-5">フォロー　 <span class="badge badge-light badge-pill">32</span></button>
         <button type="button" class="btn btn-info ml-5">フォロワー <span class="badge badge-light badge-pill">21</span></button>
     </div>
             
@@ -60,7 +54,7 @@
         <div class="w-100"></div>
 
         
-        <div class="col-9">    
+        <div class="col-md-9 col-12">    
             <ul class="nav nav-tabs nav-justified mb-3">
                 <li class="nav-item"><a href="{{ route("users.index", ["id" => $user->id]) }}" class="nav-link {{ Request::is("users") ? "active" : "" }}">投稿 <span class="badge badge-secondary">{{ $count_posts }}</span></a></li>
                 <li class="nav-item"><a href="#" class="nav-link {{ Request::is("users/*/favorites") ? "active" : "" }}">お気に入り <span class="badge badge-secondary">32</span></a></li>
@@ -69,7 +63,7 @@
         
         
         
-        <div class="col-8">
+        <div class="col-md-8 col-12">
             @if (count($posts) > 0)
                 @include("posts.posts", ["posts" => $posts])
             @endif
